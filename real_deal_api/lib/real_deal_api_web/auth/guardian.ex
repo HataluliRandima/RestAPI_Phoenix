143486a1f6a079949b005ec82868ef1a2a9b1d63
HataluliRandima <219017847@student.uj.ac.za>
2023-08-16 08:02:56 +0200

    added gursian module

---
+++ real_deal_api/lib/real_deal_api_web/auth/guardian.ex
@@ -0,0 +1,33 @@
+defmodule RealDealApiWeb.Auth.Guardian do
+  # use this to decode and encode tokens
+  use Guardian, otp_app: :real_deal_api
+
+  alias RealDealApi.Accounts # access to account context file where we do those query to database
+
+
+  # passing json with an id
+  def subject_for_token(%{id: id}, _claims) do
+   # converting id to string
+    sub = to_string(id)
+    {:ok, sub}
+  end
+
+  # same function for having empty json and no claims
+  def subject_for_token(_, _) do
+    {:error, :no_id_provided}
+  end
+
+  def resource_from_claims(%{"sub" => id}) do
+    # we going to use account in in our token
+
+    case Accounts.get_account!(id) do
+      nil -> {:error, :not_found}
+      resource => {:ok, resource}
+    end
+  end
+
+  # for empty staff to give an error
+  def resource_from_claims(_claims) do
+    {:error, :no_id_provided}
+  end
+end
