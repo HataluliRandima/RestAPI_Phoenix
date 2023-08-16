e6d9e52a510ac68366e3adbc0cb12717a161f69c
HataluliRandima <219017847@student.uj.ac.za>
2023-08-16 05:26:11 +0200

    Done with default controller

--- real_deal_api/lib/real_deal_api_web/controllers/default_controller.ex
+++ real_deal_api/lib/real_deal_api_web/controllers/default_controller.ex
@@ -1,3 +1,10 @@
 defmodule RealDealApiWeb.DefaultController do
+ use RealDealApiWeb, :controller # importing from this meth/func
+
+
+ def index(conn, _params) do
+  text conn, "Hello , The real deal Api is live -#{Mix.env()}"
+ end
+
 
 end
