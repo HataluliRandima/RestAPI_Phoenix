caa5f3ee15a580cace50af60e97a80a570835637
HataluliRandima <219017847@student.uj.ac.za>
2023-08-16 15:06:05 +0200

    added error handling guardian

--- real_deal_api/lib/real_deal_api_web/auth/pipeline.ex
+++ real_deal_api/lib/real_deal_api_web/auth/pipeline.ex
@@ -1,3 +1,12 @@
 defmodule RealDealApiWeb.Auth.Pipeline do
+  use Guardian.Plug.Pipeline, otp_app: :real_deal_api,
+  # implementation for our guardian
+  module: RealDealApiWeb.Auth.Guardian,
+  # some error handler
+  error_handler: RealDealApiWeb.Auth.GuardianErrorHandler
 
+  plug Guardian.Plug.VerifySession
+  plug Guardian.Plug.VerifyHeader
+  plug Guardian.Plug.EnsureAuthenticated
+  plug Guardian.Plug.LoadResource
 end
